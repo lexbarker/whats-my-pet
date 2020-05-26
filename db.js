43a4cdc6a7edc1dd30826eb1ae8d56817e583139
Lex Barker <lbarker@localhost.localdomain>
2020-05-26 21:26:04 +0100

    ADDed db env var

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 console.log('db script');
 var mongoose = require('mongoose');
 
-const passw = process.env.DB_CRED
-const uri = 'mongodb://localhost:27017/';
-const uri = 'mongodb+srv://dbUser:${passw}@cluster0-wntrx.mongodb.net/test?retryWrites=true&w=majority'
+var passw = process.env.DB_CRED
+//console.log(passw)
+//const uri = 'mongodb://localhost:27017/';
+var uri = `mongodb+srv://dbUser:${passw}@cluster0-wntrx.mongodb.net/test?retryWrites=true&w=majority`
+//console.log(uri)
 const options = {
   useNewUrlParser: true,
   dbName: 'pets'
   };
 mongoose.connect(uri, options);
+
